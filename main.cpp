e23ac3a3b66353a8627bcafd72b5f0c4c3401a52
xujunforce <cnxujunforce@gmail.com>
2024-12-01 14:47:53 +0800

    finish

---
+++ main.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int len = nums.size();
+        int left = 0;
+        for(int right = 0;right<len;right++){
+            if(nums[right]!=val){
+                nums[left] = nums[right];
+                left++;
+            }
+        }
+        return left;
+    }
+};
